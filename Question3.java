77012eb4a3d1a9322475546861959e6365527dfd
aidancjh <130948612+aidancjh@users.noreply.github.com>
2023-04-17 11:32:37 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -4,13 +4,10 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
-     
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter an integer: ");
+    int Int = in.nextInt();
+    int prod = Int * Int;
+    System.out.println("The answer is " + prod);
   }
 }
