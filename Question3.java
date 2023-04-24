d739248f66ec73a899b2965f53d9c4583008e61d
aidancjh <130948612+aidancjh@users.noreply.github.com>
2023-04-24 10:19:44 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -5,9 +5,7 @@ public class Question3
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter an integer: ");
     int Int = in.nextInt();
     int prod = Int * Int;
-    System.out.println("The answer is " + prod);
   }
 }
