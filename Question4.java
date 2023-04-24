1083ee4e0b843718955424a6a830fe8b6ed4efe7
aidancjh <130948612+aidancjh@users.noreply.github.com>
2023-04-25 00:37:36 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -5,7 +5,7 @@ public class Question4
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
-        int number = input.nextInt();
+        int number = in.nextInt();
 
             for (int i = number; i >= 1; i--) {
                 for (int j = 1; j <= i; j++) {
