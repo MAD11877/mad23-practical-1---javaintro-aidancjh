fe68dd6f4e8030b6da0536a1350ac33098d3c7e3
aidancjh <130948612+aidancjh@users.noreply.github.com>
2023-04-24 23:55:41 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -5,14 +5,17 @@ public class Question4
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
-    int num = in.NextInt();
+    System.out.print("Enter a number: ");
+        int number = input.nextInt();
 
-    for (int i = num; i >= 1; i--) 
-    {
-      for (int j = 1; j <= i; j++) 
-      {
-          System.out.print("*");
-      }
-    } 
+        while (number > 0) {
+            for (int i = number; i >= 1; i--) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            number--;
+        }
   }
 }
